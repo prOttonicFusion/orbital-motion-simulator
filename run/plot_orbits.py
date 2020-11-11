8e2c1d5bcd8e2c8e41b333593a2fa9064dfc2f9b
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-12 00:41:13 +0200

    add plotting script

---
+++ run/plot_orbits.py
@@ -0,0 +1,39 @@
+# Script for visualizing simulation *.dat output files
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+if (len(sys.argv) != 2):
+    print("Usage: {} <output-file-path>".format(sys.argv[0]))
+    exit()
+
+dataFilePath = sys.argv[1]
+data = np.loadtxt(dataFilePath)
+
+objectIDList = np.unique(data[:,0]) 
+objectCount = np.size(objectIDList)
+labels = list(map(lambda id: 'Object #{:d}'.format(int(id)), objectIDList))
+plotStyles1 =  ['ro','b-','g-','c-','m-','r--','b--','g--','c--']
+plotStyles2 =  ['r-','b-','g-','c-','m-','r--','b--','g--','c--']
+
+# # Plot y(x,z) graph:
+fig = plt.figure(1)
+ax = plt.axes(projection='3d')
+
+for i in range(0, objectCount):
+    ax.scatter3D(data[i:-1:objectCount, 1], data[i:-1:objectCount, 2], data[i:-1:objectCount, 3], plotStyles1[i])
+plt.xlabel('X,  [AU]')
+plt.ylabel('Y,  [AU]')
+plt.legend(labels)
+plt.show()
+
+# # Plot x(t) graph:
+fig = plt.figure(2)
+ax = plt.axes()
+for i in range(0, objectCount):
+    plt.plot(data[i:-1:objectCount, 4], data[i:-1:objectCount, 1], plotStyles2[i])
+plt.xlabel('Time, [s]')
+plt.ylabel('X, [AU]')
+plt.legend(labels)
+plt.show()
